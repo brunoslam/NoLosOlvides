5febfd063f7196e1e3debf56176c3891b9713328
Bruno Alonso Palma Ávila <bpalma@LATINSHARE.COM>
2020-06-06 01:41:02 -0400

    ingresar caso 0.5

--- src/views/ingresar-caso/ingresarCaso.js
+++ src/views/ingresar-caso/ingresarCaso.js
@@ -11,12 +11,23 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 import CloseIcon from '@material-ui/icons/Close';
-import GridItem from "components/Grid/GridItem.js";
-import GridContainer from "components/Grid/GridContainer.js";
 import ReactDataGrid from 'react-data-grid';
 import Categoria from "model/categoria";
 import Cargos from "model/cargos";
-
+import {
+    Button,
+    Label,
+    FormGroup,
+    Input,
+    NavItem,
+    NavLink,
+    Nav,
+    TabContent,
+    TabPane,
+    Container,
+    Row,
+    Col
+} from "reactstrap";
 const columns = [
     { key: "id", name: "#", editable: false },
     { key: "titulo", name: "Título", editable: true },
@@ -37,6 +48,8 @@ export default class ingresarCaso extends Component {
         };
         this.onGridRowsUpdated.bind(this);
         this.getCellActions.bind(this);
+        this.validarFormulario.bind(this);
+        this.insertarPersonaje.bind(this);
     }
 
     async componentWillMount() {
@@ -76,6 +89,46 @@ export default class ingresarCaso extends Component {
         return column.key === "action" ? cellActions : null;
     };
 
+    async validarFormulario() {
+        var flagFormValido = true;
+
+        var nombre = (document.getElementById("txtNombrePersonaje")).value;
+        var apellido = (document.getElementById("txtApellidoPersonaje")).value;
+        var descripcion = (document.getElementById("txtDescripcionPersonaje")).value;
+        var rut = (document.getElementById("txtRutPersonaje")).value;
+        var nacionalidad = (document.getElementById("txtNacionalidadPersonaje")).value;
+        var cargo = (document.getElementById("selectCargoPersonaje")).value;
+        var imagen = (document.getElementById("txtImagenPersonaje")).value;
+
+
+        if (!nombre || !apellido || !cargo) {
+            alert("Debes completar los campos obligatorios");
+            flagFormValido = false;
+        }
+
+        if (this.state.rows.length == 0) {
+            alert("Debes ingresar al menos una evidencia");
+            flagFormValido = false;
+        } else {
+            this.state.rows.map((row) => {
+                if (!row.titulo || !row.descripcion || !row.fecha || !row.link) {
+                    alert("Debes compeltar todos los datos de la tabla");
+                    flagFormValido = false;
+                }
+            });
+        }
+
+        if (flagFormValido) {
+            this.insertarPersonaje();
+        }
+
+
+    }
+
+    async insertarPersonaje() {
+        alert(this);
+    }
+
     onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
         this.setState(state => {
             const rows = state.rows.slice();
@@ -89,35 +142,76 @@ export default class ingresarCaso extends Component {
     render() {
         return (
             <TemplateNoLosOlvides>
-                <GridContainer>
-                    <GridItem xs={12} sm={12} md={12} spacing={2} justify="space-around">
-                        <p>*Nombre: <input /></p>
-                        <p>*Apellido: <input /></p>
-                        <p>Rut: <input /></p>
-                        <p>Nacionalidad: <input /></p>
-                        <p>Cargo: <select >
-                            {this.state.cargos.map((cargo) => {
-                                return (<option>{cargo.titulo}</option>);
-                            })}
-                        </select></p>
-                        <p>Imágen: <input type="file" /></p>
+                <Container>
+                    <Row>
+                        <Col className="ml-auto mr-auto" md="6">
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">*Nombre:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><input id="txtNombrePersonaje" /></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">*Apellido:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><input id="txtApellidoPersonaje" /></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">Descripción:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><textarea id="txtDescripcionPersonaje"></textarea ></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">Rut:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><input id="txtRutPersonaje" /></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">Nacionalidad:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><input id="txtNacionalidadPersonaje" /></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">Cargo:</Col>
+                                <Col className="ml-auto mr-auto" md="6"><select id="selectCargoPersonaje">
+                                    {this.state.cargos.map((cargo) => {
+                                        return (<option value={cargo.idCargo}>{cargo.titulo}</option>);
+                                    })}
+                                </select></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6">Imágen:</Col>
+                                {/* <Col className="ml-auto mr-auto" md="6"><input type="file" /></Col> */}
+                                <Col className="ml-auto mr-auto" md="6"><input id="txtImagenPersonaje" /></Col>
+                            </Row>
+                            <Row>
+                                <Row><Col className="" md="2">Evidencias:</Col></Row>
+                                <Row>
+                                    <Col className="" md="2"><button onClick={this.handleAddRow.bind(this)}>+</button></Col>
+                                    <Col className="" md="2"><button>-</button></Col>
+                                </Row>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="12">
+                                    <ReactDataGrid
+                                        columns={columns}
+                                        rowGetter={i => this.state.rows[i]}
+                                        rowsCount={this.state.rows.length}
+                                        onGridRowsUpdated={this.onGridRowsUpdated}
+                                        enableCellSelect={true}
+                                        getCellActions={this.getCellActions}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6"><Button onClick={() => { this.validarFormulario(); }}>Guardar</Button></Col>
+                            </Row>
+                            <Row>
+                                <Col className="ml-auto mr-auto" md="6"></Col>
+                                <Col className="ml-auto mr-auto" md="6"></Col>
+                            </Row>
 
 
-                        <button onClick={this.handleAddRow.bind(this)}>+</button>
-                        <button>-</button>
 
-                        <GridItem xs={12} sm={12} md={8} spacing={2} justify="space-around">
-                            <ReactDataGrid
-                                columns={columns}
-                                rowGetter={i => this.state.rows[i]}
-                                rowsCount={this.state.rows.length}
-                                onGridRowsUpdated={this.onGridRowsUpdated}
-                                enableCellSelect={true}
-                                getCellActions={this.getCellActions}
-                            />
-                        </GridItem>
-                    </GridItem>
-                </GridContainer>
+
+
+                        </Col>
+                    </Row >
+                </Container>
             </TemplateNoLosOlvides >
         )
     }
